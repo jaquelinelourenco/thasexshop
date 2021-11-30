4ea348ee855565b4ceb82c73fa355438b2180361
Jaqueline Lourenço <jaquelineloure50@gmail.com>
2021-11-30 09:29:46 -0300

    :memo: Inserindo rotas
    Inserindo rotas

--- thasexyshop-app/src/routes/router.js
+++ thasexyshop-app/src/routes/router.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import {BrowserRouter as Routes, Route, Router} from "react-router-dom"
+import HomePage from "../pages/HomePage"
+import CartPage from "../pages/CartPage"
+import LoginPage from "../pages/LoginPage"
+
+export const RouterWrapp = () => {
+    return(
+        <Router>
+            <Routes>
+                <Route path='../pages/LoginPage.js'>
+                    element={ <LoginPage /> }
+                </Route>
+
+                <Route path='../pages/HomePage.js'>
+                    element={ <HomePage /> }
+                </Route>
+
+                <Route path='../pages/CartPage.js'>
+                    element={ <CartPage /> }
+                </Route>
+                <Route>
+                    "Erro 404 - Página não existe!"
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
